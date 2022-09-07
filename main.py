2cb502360792a12263aca9a27e4ee0a801ae6628
zjac082@live.rhul.ac.uk <83787341+XLmT09@users.noreply.github.com>
2022-09-07 20:33:25 +0100

    add boundaries

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ WIDTH, HEIGHT = 1000, 500
 ROAD_WIDTH, ROAD_HEIGHT = 100, 200
 PARK_WIDTH, PARK_HEIGHT = 75, 150
 LIGHT_GREEN = (68, 255, 0)
+BLACK = (0, 0, 0)
 
 
 ROAD = pygame.image.load(os.path.join("assets", "road.png"))
@@ -13,6 +14,13 @@ PARK = pygame.transform.rotate(pygame.transform.scale(
 ROAD_ONE = pygame.transform.scale(ROAD ,(ROAD_WIDTH, ROAD_HEIGHT))
 ROAD_TWO = pygame.transform.rotate(pygame.transform.scale(ROAD ,(ROAD_WIDTH, ROAD_HEIGHT * 3)), 90)
 
+BOUNDARY_ONE = pygame.Rect(199, HEIGHT - ROAD_WIDTH - ROAD_HEIGHT, 1, 300)
+BOUNDARY_TWO = pygame.Rect(299, HEIGHT - ROAD_HEIGHT, 1, 200)
+BOUNDARY_THREE = pygame.Rect(199, HEIGHT - ROAD_HEIGHT - ROAD_WIDTH, ROAD_HEIGHT * 3 - PARK_WIDTH * 2, 1)
+BOUNDARY_FOUR = pygame.Rect(299, HEIGHT - ROAD_HEIGHT, ROAD_HEIGHT * 3 - ROAD_WIDTH, 1)
+BOUNDARY_FIVE = pygame.Rect(799, 125, 1, ROAD_WIDTH + PARK_WIDTH)
+BOUNDARY_SIX = pygame.Rect(650, 125, PARK_HEIGHT, 1)
+BOUNDARY_SEVEN = pygame.Rect(650, 125, 1, PARK_WIDTH)
 
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 
@@ -22,6 +30,13 @@ def draw():
     WIN.blit(ROAD_ONE, (200 , HEIGHT - ROAD_HEIGHT))
     WIN.blit(ROAD_TWO, (200 , HEIGHT - ROAD_WIDTH - ROAD_HEIGHT))
     WIN.blit(PARK, (650, HEIGHT - ROAD_WIDTH - ROAD_HEIGHT - PARK_WIDTH))
+    pygame.draw.rect(WIN, BLACK, BOUNDARY_ONE)
+    pygame.draw.rect(WIN, BLACK, BOUNDARY_TWO)
+    pygame.draw.rect(WIN, BLACK, BOUNDARY_THREE)
+    pygame.draw.rect(WIN, BLACK, BOUNDARY_FOUR)
+    pygame.draw.rect(WIN, BLACK, BOUNDARY_FIVE)
+    pygame.draw.rect(WIN, BLACK, BOUNDARY_SIX)
+    pygame.draw.rect(WIN, BLACK, BOUNDARY_SEVEN)
 
 def main():
     run = True
