92f066618b485e84646704b846d88918c6ce17d9
zjac082@live.rhul.ac.uk <83787341+XLmT09@users.noreply.github.com>
2022-09-07 19:35:56 +0100

    put objects on game

--- main.py
+++ main.py
@@ -1,8 +1,27 @@
 import pygame
+import os
 
 WIDTH, HEIGHT = 1000, 500
+ROAD_WIDTH, ROAD_HEIGHT = 100, 200
+PARK_WIDTH, PARK_HEIGHT = 75, 150
+LIGHT_GREEN = (68, 255, 0)
+
+
+ROAD = pygame.image.load(os.path.join("assets", "road.png"))
+PARK = pygame.transform.rotate(pygame.transform.scale(
+    pygame.image.load(os.path.join("assets", "park.png")), (PARK_WIDTH, PARK_HEIGHT)), 90)
+ROAD_ONE = pygame.transform.scale(ROAD ,(ROAD_WIDTH, ROAD_HEIGHT))
+ROAD_TWO = pygame.transform.rotate(pygame.transform.scale(ROAD ,(ROAD_WIDTH, ROAD_HEIGHT * 3)), 90)
+
 
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+
+def draw():
+    pygame.display.update()
+    WIN.fill(LIGHT_GREEN)
+    WIN.blit(ROAD_ONE, (200 , HEIGHT - ROAD_HEIGHT))
+    WIN.blit(ROAD_TWO, (200 , HEIGHT - ROAD_WIDTH - ROAD_HEIGHT))
+    WIN.blit(PARK, (650, HEIGHT - ROAD_WIDTH - ROAD_HEIGHT - PARK_WIDTH))
 
 def main():
     run = True
@@ -10,7 +29,8 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
-    
+
+        draw()    
     pygame.quit()
 
 if __name__ == "__main__":
