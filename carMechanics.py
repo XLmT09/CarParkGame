44af200b18850fe1f57ba6a041c66ac0cf0c187f
zjac082@live.rhul.ac.uk <83787341+XLmT09@users.noreply.github.com>
2022-09-08 18:52:56 +0100

    add car to game and basic movement

--- carMechanics.py
+++ carMechanics.py
@@ -1,78 +1,69 @@
 from operator import truediv
 import pygame
-import time
-import math
 import os
 from utils import  scale_image, blit_rotate_center, rot_center
 
+#WIDTH, HEIGHT = 500,500
+#WIN = pygame.display.set_mode((WIDTH,HEIGHT))
+#pygame.display.set_caption("PARKING GAME")
+#greenCar = scale_image(pygame.image.load(os.path.join('assets', 'GreenCar.png')),0.1)
+#FPS = 120
 
-GreenCar = scale_image(pygame.image.load(os.path.join('assets', 'GreenCar.png')),0.1)
-
-WIDTH, HEIGHT = 500,500
-WIN = pygame.display.set_mode((WIDTH,HEIGHT))
-pygame.display.set_caption("PARKING GAME")
-
-FPS = 60
-
-
-class AbstractCar:
-    
-    def __init__(self, max_vel,  rotation_vel):
-        self.img = self.IMG
-
-        self.max_vel = max_vel
-        self.vel = 0
-        self.rotation_vel = rotation_vel
-        self.angle = 0
-        self.x, self.y = self.START_POS
+class PlayerCar(pygame.sprite.Sprite):
+    def __init__(self, x, y, vel):
+        pygame.sprite.Sprite.__init__(self)
+        self.images = []
+        img = scale_image(pygame.image.load(os.path.join('assets', 'GreenCar.png')).convert(), 0.1)
+        self.images.append(img)
+        self.image = self.images[0]
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+        self.vel = vel
     
     def rotate(self, left= False, right = False):
         if left:
             self.angle += self.rotation_vel
         elif right:
             self.angle -= self.rotation_vel
-    def draw(self,win):
-        blit_rotate_center(win, self.img, (self.x, self.y), self.angle)
-        pygame.display.update()
+
+    def move(self):
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_d]:
+            self.rect.x += self.vel
+        if keys[pygame.K_a]:
+            self.rect.x -= self.vel
+        if keys[pygame.K_w]:
+            self.rect.y -= self.vel
+        if keys[pygame.K_s]:
+            self.rect.y += self.vel
+        
+#    def draw(self,win):
+#        blit_rotate_center(win, self.img, (self.x, self.y), self.angle)
+#        pygame.display.update()
+
+#def draw(win, images, player_car):
+#    for img, pos in images:
+#        win.blit(img, pos)
+#    player_car.draw(win)
+#    pygame.display.update()
+
+
+#run = True
+#clock = pygame.time.Clock()
+#player_car = PlayerCar(4, 4, greenCar, (180,200))
+
+# while run:
+#     clock.tick(FPS)
+#     for event in pygame.event.get():
+#         if event.type == pygame.QUIT:
+#             run = False
+#             break
     
-
-
-class PlayerCar(AbstractCar):
-    IMG = GreenCar
-    START_POS = (180,200)
-
-def draw(win, images, player_car):
-    for img, pos in images:
-        win.blit(img, pos)
-    player_car.draw(win)
-    pygame.display.update()
-
-
-run = True
-clock = pygame.time.Clock()
-images = []
-player_car = PlayerCar(4,4)
-
-
-
-
-
-while run:
-    clock.tick(FPS)
-
-    draw(WIN, images, player_car )
-
-
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            run = False
-            break
-    
-    keys = pygame.key.get_pressed()
-    if keys[pygame.K_a]:
-        player_car.rotate(left=True)
-    if keys[pygame.K_d]:
-        player_car.rotate(right=True)
-
-pygame.quit()
+#     #keys = pygame.key.get_pressed()
+#     car.move()
+#     #if keys[pygame.K_d]:
+#     #    car.rotate(right=True)
+#     pygame.display.update()
+# pygame.quit()
 
--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import pygame, os, button, objects, sys
+import pygame, os, button, objects, sys, carMechanics
 pygame.init()
 WIDTH, HEIGHT = 1000, 500
-
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+FPS = 60
 
 ROAD_WIDTH, ROAD_HEIGHT = 100, 200
 PARK_WIDTH, PARK_HEIGHT = 75, 150
@@ -18,32 +18,43 @@ PARK = pygame.transform.rotate(pygame.transform.scale(
 ROAD_ONE = pygame.transform.scale(ROAD ,(ROAD_WIDTH, ROAD_HEIGHT))
 ROAD_TWO = pygame.transform.rotate(pygame.transform.scale(ROAD ,(ROAD_WIDTH, ROAD_HEIGHT * 3)), 90)
 
+clock = pygame.time.Clock()
+
 start_btn = button.Button(375, 100, START_IMG, 1)
 quit_btn = button.Button(375, 250, QUIT_IMG, 1)
 quit2_btn = button.Button(0, 0, QUIT2_IMG, 0.05)
 
+car = carMechanics.PlayerCar(40, 40, 1)   
+player_list = pygame.sprite.Group()
+player_list.add(car)
+
 def draw_level_one():
     run = True
+    clock.tick(FPS)
     while run:
         WIN.blit(BACKGROUND, (0, 0))
         WIN.blit(ROAD_ONE, (200 , HEIGHT - ROAD_HEIGHT))
         WIN.blit(ROAD_TWO, (200 , HEIGHT - ROAD_WIDTH - ROAD_HEIGHT))
         WIN.blit(PARK, (650, HEIGHT - ROAD_WIDTH - ROAD_HEIGHT - PARK_WIDTH))
+        
         for bound in objects.lvl1_boundaries:
             pygame.draw.rect(WIN, BLACK, bound)
 
+        player_list.draw(WIN)
+        car.move()
         if quit2_btn.draw(WIN):
             main_menu()
         
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
-            
+        
         pygame.display.update()
     pygame.quit()
     sys.exit()
 
 def main_menu():
+    clock.tick(FPS)
     run = True
     while run:
         WIN.fill((153, 204, 255))
